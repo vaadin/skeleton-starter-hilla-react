d3c77851036ddcc9dca9f3e029893a0e61b44340
Vlad Rindevich <vladrin@vaadin.com>
2024-03-19 12:50:41 +0200

    refactor: use document title

--- src/main/frontend/views/$layout.tsx
+++ src/main/frontend/views/$layout.tsx
@@ -9,16 +9,20 @@ const navLinkClasses = ({ isActive }: any) => {
   return `block rounded-m p-s ${isActive ? 'bg-primary-10 text-primary' : 'text-body'}`;
 };
 
-export default function MainLayout() {
-  const currentTitle = useViewConfig()?.title ?? 'Hybrid example';
+const defaultTitle = document.title;
 
-  useEffect(() => {document.title = currentTitle;}, [currentTitle]);
+export default function MainLayout() {
+  const currentTitle = useViewConfig()?.title ?? defaultTitle;
+
+  useEffect(() => {
+    document.title = currentTitle;
+  }, [currentTitle]);
 
   return (
     <AppLayout primarySection="drawer">
       <div slot="drawer" className="flex flex-col justify-between h-full p-m">
         <header className="flex flex-col gap-m">
-          <h1 className="text-l m-0">Hybrid example</h1>
+          <h1 className="text-l m-0">${currentTitle}</h1>
           <nav>
             {createMenuItems().map(({ to, icon, title }) => (
               <NavLink className={navLinkClasses} to={to} key={to}>
