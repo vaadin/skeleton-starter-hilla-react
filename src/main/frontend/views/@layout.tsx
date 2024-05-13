be8e88b2d2317c4704e7c255a46a52b33b7e4e72
Mikhail Shabarov <mikhail@vaadin.com>
2024-05-13 13:00:37 +0300

    fix: Use signals for page title

--- src/main/frontend/views/@layout.tsx
+++ src/main/frontend/views/@layout.tsx
@@ -9,45 +9,51 @@ import {
   createMenuItems,
   useViewConfig,
 } from "@vaadin/hilla-file-router/runtime.js";
-import { Suspense, useEffect } from "react";
+import { Suspense } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Signal, signal, effect } from '@vaadin/hilla-react-signals';
 
-const defaultTitle = document.title;
+const vaadin = window.Vaadin as {
+  documentTitleSignal: Signal<string>;
+};
+vaadin.documentTitleSignal = signal("");
+effect(() =>  document.title = vaadin.documentTitleSignal.value);
 
 export default function MainLayout() {
-  const currentTitle = useViewConfig()?.title ?? defaultTitle;
+  const currentTitle = useViewConfig()?.title ?? '';
   const navigate = useNavigate();
   const location = useLocation();
 
-  useEffect(() => {
-    document.title = currentTitle;
-  }, [currentTitle]);
+  vaadin.documentTitleSignal.value = currentTitle;
 
   return (
-    <AppLayout primarySection="drawer">
-      <div slot="drawer" className="flex flex-col justify-between h-full p-m">
-        <header className="flex flex-col gap-m">
-          <h1 className="text-l m-0">{currentTitle}</h1>
-          <SideNav
-            onNavigate={({ path }) => navigate(path!)}
-            location={location}
-          >
-            {createMenuItems().map(({ to, icon, title }) => (
-              <SideNavItem path={to} key={to}>
-                {title}
-              </SideNavItem>
-            ))}
-          </SideNav>
-        </header>
-      </div>
+      <AppLayout primarySection="drawer">
+        <div slot="drawer" className="flex flex-col justify-between h-full p-m">
+          <header className="flex flex-col gap-m">
+            <h1 className="text-l m-0">{currentTitle}</h1>
+            <SideNav
+                onNavigate={({path}) => navigate(path!)}
+                location={location}
+            >
+              {createMenuItems().map(({to, icon, title}) => (
+                  <SideNavItem path={to} key={to}>
+                    {title}
+                  </SideNavItem>
+              ))}
+            </SideNav>
+          </header>
+        </div>
 
-      <DrawerToggle slot="navbar" aria-label="Menu toggle"></DrawerToggle>
+        <DrawerToggle slot="navbar" aria-label="Menu toggle"></DrawerToggle>
+        <h2 slot="navbar" className="text-l m-0">
+          {vaadin.documentTitleSignal}
+        </h2>
 
-      <Suspense fallback={<ProgressBar indeterminate className="m-0" />}>
-        <section className="view">
-          <Outlet />
-        </section>
-      </Suspense>
-    </AppLayout>
+        <Suspense fallback={<ProgressBar indeterminate className="m-0"/>}>
+          <section className="view">
+            <Outlet/>
+          </section>
+        </Suspense>
+      </AppLayout>
   );
 }
