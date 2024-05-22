cad78e50a9344407b1c0bf3c1cee5c3e4502ccc8
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2024-05-22 09:38:12 +0300

    fix: Use signals for page title (#231)
    Uses signal for dynamically updated page title.
    
    Part of https://github.com/vaadin/flow/issues/19200

--- src/main/frontend/views/@index.tsx
+++ src/main/frontend/views/@index.tsx
@@ -1,6 +1,13 @@
 import { Button, Notification, TextField } from "@vaadin/react-components";
 import { HelloEndpoint } from "Frontend/generated/endpoints.js";
 import { useState } from "react";
+import type { ViewConfig } from "@vaadin/hilla-file-router/types.js";
+
+export const config: ViewConfig = {
+    menu: {
+        title: "Main page"
+    }
+};
 
 export default function MainView() {
   const [name, setName] = useState("");
